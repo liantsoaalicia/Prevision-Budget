fb482f8f805b1ad735971c38d68af0e0ba499bac
Eidnar0206 <randieranaivoson@gmail.com>
2025-03-23 19:08:57 +0300

    cssheheeee

--- pages/login.php
+++ pages/login.php
@@ -1,30 +1,39 @@
-<?php 
-    require('../inc/fonction.php');
-
-    $departements = getAllDepartements();
+<?php
+require('../inc/fonction.php');
+$departements = getAllDepartements();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="../assets/login.css"> 
 </head>
 <body>
-    <?php if(isset($_GET['error'])) { ?>
-        <h3 style="color: red;"><?= $_GET['error'] ?></h3>
-    <?php } ?>
-
-    <form action="traitement-login.php" method="post">
-        <label for="nom">Nom <select name="idDepartement" id="nom">
-        <option value="#">Selectionnez un departement</option>
-            <?php foreach($departements as $d) { ?>
-                <option value="<?= $d['idDepartement'] ?>"><?= $d['nom'] ?></option>
-            <?php } ?>
-        </select></label>
-        <label for="mdp">Mot de passe <input type="password" name="mdp" id="mdp"></label>
-        <button type="submit">Se connecter</button>
-    </form>
+    <div class="formulaire-login"> 
+        <?php if(isset($_GET['error'])) { ?>
+            <h3 style="color: red;"><?= $_GET['error'] ?></h3>
+        <?php } ?>
+        
+        <form action="traitement-login.php" method="post">
+            <label for="nom">
+                Nom 
+                <select name="idDepartement" id="nom">
+                    <option value="#">Selectionnez un departement</option>
+                    <?php foreach($departements as $d) { ?>
+                        <option value="<?= $d['idDepartement'] ?>"><?= $d['nom'] ?></option>
+                    <?php } ?>
+                </select>
+            </label>
+            
+            <label for="mdp">
+                Mot de passe 
+                <input type="password" name="mdp" id="mdp">
+            </label>
+            
+            <button type="submit">Se connecter</button>
+        </form>
+    </div> >
 </body>
 </html>
