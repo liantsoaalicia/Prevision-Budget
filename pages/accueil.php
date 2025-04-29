843ab0265f9ca07dbc8bc3b6d501da4f16220a67
liantsoaalicia <liantsoaalicia@gmail.com>
2025-04-29 11:22:12 +0200

    oko

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,21 +1,18 @@
 <section id="menu-description">
     <h1 class="main-title">Gestion Budgétaire</h1>
     <div class="menu-sections">
-        <!-- Départements -->
         <div class="menu-item">
             <h3>Départements</h3>
             <p>Gestion complète des départements : </p>
             <p>liste, ajout, budget et suivi des activités.</p>
         </div>
         
-        <!-- Catégories -->
         <div class="menu-item">
             <h3>Catégories</h3>
             <p>Organisation et classification </p>
             <p>des catégories pour un suivi efficace.</p>
         </div>
         
-        <!-- Budget -->
         <div class="menu-item">
             <h3>Budget</h3>
             <p>Gestion des prévisions, suivi </p>
